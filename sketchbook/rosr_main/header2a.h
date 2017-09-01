afcc00104a31fa674ae82f5d243501f2d4b78b4c
mike <michael@rmrco.com>
2017-09-01 12:15:01 -0700

    dev

--- sketchbook/rosr_main/header2a.h
+++ sketchbook/rosr_main/header2a.h
@@ -1,4 +1,5 @@
+//v31-2a we swapped the bb1 and bb2 as a test
 #define EDITDATE    "20170901T152848Z" 
-#define VERSION     "31-2"
+#define VERSION     "31-2a"
 #define PROGRAMNAME "rosr_main"
 const byte  EEPROM_ID = 16;  //!! change if you fool around with eeprom variables
--- sketchbook/rosr_main/header3.h
+++ sketchbook/rosr_main/header3.h
@@ -1,2 +1,4 @@
-#define PROGRAMNAME "rosr_main_r3"
+#define VERSION     "31-3"
+#define EDITDATE    "20170901T152848Z" 
+#define PROGRAMNAME "rosr_main"
 const byte  EEPROM_ID = 17;  //!! change if you fool around with eeprom variables
