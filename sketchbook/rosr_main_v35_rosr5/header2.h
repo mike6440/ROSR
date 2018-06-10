0d16c65aaf4660e9e0be85e1a3781252d2e67baf
mike <michael@rmrco.com>
2018-06-10 11:41:11 -0700

    rosr sw ver 35

---
+++ sketchbook/rosr_main_v35_rosr5/header2.h
@@ -0,0 +1,4 @@
+#define EDITDATE    "20170901T152848Z" 
+#define VERSION     "31-2"
+#define PROGRAMNAME "rosr_main"
+const byte  EEPROM_ID = 16;  //!! change if you fool around with eeprom variables
---
+++ sketchbook/rosr_main_v35_rosr5/header3.h
@@ -0,0 +1,6 @@
+#define VERSION     "33" //
+#define FORMAT "3" //1-F 
+#define SERIALNUMBER "03"  // two digits
+#define EDITDATE    "20180409T202304Z" 
+#define PROGRAMNAME "rosr_main"
+const byte  EEPROM_ID = 17;  //!! change if you fool around with eeprom variables
