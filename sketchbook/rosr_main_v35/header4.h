cb98ea6373e476209b3bdbb910ab29363b9de727
Michael Reynolds <michael@rmrco.com>
2019-08-20 18:00:57 -0700

    prep rosr4

--- sketchbook/rosr_main_v35/header4.h
+++ sketchbook/rosr_main_v35/header4.h
@@ -1,6 +1,6 @@
-#define VERSION     "32"
-#define EDITDATE    "170918" 
+#define VERSION     "35"
+#define EDITDATE    "190819" 
 #define PROGRAMNAME "rosr_main"
 const byte  EEPROM_ID = 17;  //!! change if you fool around with eeprom variables
 #define FORMAT "2" //1-F 
-#define SERIALNUMBER "04";  // two digits
+#define SERIALNUMBER "04"  // two digits
